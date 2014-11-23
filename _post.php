30346e2fabf295c5f8180e691bc5879f5c73bcf8
gadgetevil <mike.rodia@gmail.com>
2014-11-22 22:15:56 -0500

    del;ete

--- _post.php
+++
@@ -1,9 +0,0 @@
-<?php
-$html = str_replace(array(chr(10), chr(13), ':', ' '),'', $_POST['chan']) . ' ' . str_replace(array(chr(10), chr(13), ':', ' '),'',$_POST['name']) . ' ' . $_POST['msg'];
-$file = "post.txt"; 
-$handle = fopen($file,"a");
-fwrite($handle,$html);
-fclose($handle);
-header("Location:" . $_POST['link']);
-exit();
-?>
